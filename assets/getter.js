12d047477e222c67b302ed17ef5d9c1ae5261843
ethch18 <ankimoponzu@hotmail.com>
2016-05-22 01:20:19 -0700

    finally got the json to work

--- assets/getter.js
+++ assets/getter.js
@@ -10,39 +10,6 @@
 	xhttp.send();
 }*/
 
-//jquery functions: api ping and hover coloration for clear button
-$(document).ready(function() {
-	$("#searchbutton").click(function() {
-		$.get("http://food2fork.com/api/search?key=c8b4412d38aeeade93c0bab3425f24ce&q=bread", function(data) {
-			var actualdata = data.substring(24, data.length - 1);
-			document.getElementById("statement").innerHTML = "";
-			var datatoparse = JSON.parse(actualdata);
-			var unparsedingredients = document.getElementById("list");
-			var parseddata = "";
-			var postedingredients = "";
-			for (var key in datatoparse) {
-				//you can (and should!) change this
-				//parseddata += datatoparse[key]["publisher"] + " " + datatoparse[key]["title"] + " " + datatoparse[key]["source_url"] + "BREAK" + '<br/>';
-				
-				parseddata += ('<li class="WHATEVER">'
-							+'<h1><a href="'+datatoparse[key]["source_url"]+'">'+datatoparse[key]["title"]+'</h1>'
-							+'<img src="'+datatoparse[key]["image-url"]+'"></img>'
-							+'<h2>'+datatoparse[key]["publisher"]+'</h2>'
-							+'<h3>'+datatoparse[key]["social_rank"]+'</h3>'
-							+'<span class="SOMETHING">'+datatoparse[key]["social_rank"]+'</span></li>');
-			}
-			document.getElementById("postedlist").innerHTML += postedingredients;
-			document.getElementById("recipeList").innerHTML += parseddata;
-		});
-	});
-	$(".clearer").hover(function(){
-		document.getElementById("clearbutton").style.backgroundColor = "grey";
-	},
-	function() {
-		document.getElementById("clearbutton").style.backgroundColor = "#999999";
-	});
-});
-
 //checks to see if keypress is enter
 function checkForEnter() {
 	if (event.keyCode == 13)
@@ -65,3 +32,58 @@ function addToList() {
 function clearList() {
 	document.getElementById("list").innerHTML = "";
 }
+
+
+//jquery functions: api ping and hover coloration for clear button
+$(document).ready(function() {
+	$("#searchbutton").click(function() { //TODO: grab list of ingredients, sorting
+		//$.get("http://food2fork.com/api/search?key=c8b4412d38aeeade93c0bab3425f24ce&q=", function(data) {
+		$.get("https://cz9dldanfb.execute-api.us-east-1.amazonaws.com/prod/ingredient/bread", function(data) {
+			//console.log(data);
+			document.getElementById("postedlist").innerHTML = "";
+			document.getElementById("recipelist").innerHTML = "";
+			var unparsedingredients = document.getElementsByClassName("libg");
+			var parseddata = "";
+			var postedingredients = "<p>";
+			
+			for (var i = 0; i < unparsedingredients.length; i++) {
+				//add each ingredient in
+				postedingredients += ('<span class="libg">'+item.innerHTML+'</span>'+" ")
+			}
+			//TODO: this is broken!!
+			/*Array.from(document.getElementsByClassName("libg")).forEach(function(item) {
+				//add each ingredient in
+				postedingredients += ('<span class="libg">'+item.innerHTML+'</span>'+" ")
+			}*/
+			postedingredients += "</p>";
+			
+			/*THIS IS HOW YOU PARSE THE JSON
+			for (var i = 0; i < data["recipes"].length; i++)
+			{
+				console.log(data["recipes"][i]);
+			}
+			*/
+			
+			for (var i = 0; i < data["recipes"].length; i++) {
+				
+				parseddata += ('<li class="WHATEVER">'
+							+'<h1><a href="'+data["recipes"][i]["source_url"]+'">'+data["recipes"][i]["title"]+'</a></h1>'
+							+'<img src="'+data["recipes"][i]["image-url"]+'"></img>'
+							+'<h2>'+data["recipes"][i]["publisher"]+'</h2>'
+							+'<h3>Social Rank</h3>'
+							+'<span class="SOMETHING">'+data["recipes"][i]["social_rank"]+'</span></li>');
+			}
+			var modalobject = document.getElementById("recipeModal")
+			document.getElementById("postedlist").innerHTML += postedingredients;
+			document.getElementById("recipelist").innerHTML += parseddata;
+			modalobject.style.display = "block";
+		});
+	});
+	$(".clearer").hover(function(){
+		document.getElementById("clearbutton").style.backgroundColor = "grey";
+	},
+	function() {
+		document.getElementById("clearbutton").style.backgroundColor = "#999999";
+	});
+});
+
