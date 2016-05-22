fa9cecb673c0ca2fe4b564fa16e4c6d991477f0a
ethch18 <ankimoponzu@hotmail.com>
2016-05-21 21:07:18 -0700

    finished script

--- assets/getter.js
+++ assets/getter.js
@@ -13,9 +13,14 @@
 $(document).ready(function() {
 	$("#searchbutton").click(function() {
 		$.get("http://food2fork.com/api/search?key=c8b4412d38aeeade93c0bab3425f24ce&q=bread", function(data) {
-			//document.getElementById("statement").innerHTML += data;
-			var parsedData = JSON.parse(data);
-			document.getElementById("statement").innerHTML += parsedData;
+			var actualdata = data.substring(24, data.length - 1);
+			document.getElementById("statement").innerHTML = "";
+			var datatoparse = JSON.parse(actualdata);
+			var parseddata = "";
+			for (var key in datatoparse) {
+				parseddata += datatoparse[key]["publisher"] + " " + datatoparse[key]["title"] + " " + datatoparse[key]["source_url"] + "BREAK" + '<br/>';
+			}
+			document.getElementById("statement").innerHTML += parseddata;
 		});
 	});
 });
