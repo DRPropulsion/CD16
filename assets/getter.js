40ce2905157f215ad4a74792e32a9bd6c875d7b0
ethch18 <ankimoponzu@hotmail.com>
2016-05-21 20:07:59 -0700

    updating some scripts

---
+++ assets/getter.js
@@ -0,0 +1,19 @@
+/*function getRecipes(url, callback) {
+	var xhttp;
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+            cfunc(xhttp);
+        }
+    };
+	xhttp.open("GET", url, true);
+	xhttp.send();
+}*/
+
+$(document).ready(function() {
+	$("#searchbutton").click(function() {
+		$.get("http://food2fork.com/api/search?key=c8b4412d38aeeade93c0bab3425f24ce&q=bread", function(data) {
+			document.getElementById("statement").innerHTML += data;
+		});
+	});
+});
